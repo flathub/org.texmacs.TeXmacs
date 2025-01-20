4bd47f930cdb2f1c8d3f1ef00718cfe7a703ed40
bztd <117551043+bztd@users.noreply.github.com>
2025-01-20 17:45:26 -0500

    conditioning the environment configuration to the existence of texlive

--- startTeXmacs.sh
+++ startTeXmacs.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-architecture_type=$(uname -m)
-perl_version=$(ls /app/texlive/lib/perl5/site_perl/)
-
-if ! [[ $perl_version =~ ^[0-9.]+$ ]]; then
-  echo " startTeXmacs.sh: Possible error in environment variables."
-  echo " "
+if [ "$(ls -A /app/texlive)" ]; then
+  architecture_type=$(uname -m)
+  perl_version=$(ls /app/texlive/lib/perl5/site_perl/)
+  
+  if ! [[ $perl_version =~ ^[0-9.]+$ ]]; then
+    echo " startTeXmacs.sh: Possible error in environment variables."
+    echo " "
+  fi
+  
+  export PATH=/app/texlive/bin/${architecture_type}-linux:${PATH}
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/app/texlive/lib/perl5/${perl_version}/${architecture_type}-linux/CORE
+  export PERL5LIB=/app/texlive/lib/perl5/${perl_version}:/app/texlive/lib/perl5/site_perl/${perl_version}
 fi
 
-export PATH=/app/texlive/bin/${architecture_type}-linux:${PATH}
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/app/texlive/lib/perl5/${perl_version}/${architecture_type}-linux/CORE
-export PERL5LIB=/app/texlive/lib/perl5/${perl_version}:/app/texlive/lib/perl5/site_perl/${perl_version}
 export ASPELL_CONF='dict-dir /app/share/dicts'
-
 
 exec texmacs "$@"
