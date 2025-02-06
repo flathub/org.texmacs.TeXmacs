c20c2c3df2ef9df659bf835c2a28d41db44f3431
bztd <117551043+bztd@users.noreply.github.com>
2025-02-06 14:35:24 -0500

    Correction2 startTeXmacs.sh

--- wrapper/startTeXmacs.sh
+++ wrapper/startTeXmacs.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+mkdir -p ~/.TeXmacs
+
 if [ "$(ls -A /app/texlive)" ]; then
   architecture_type=$(uname -m)
   perl_version=$(ls /app/texlive/lib/perl5/site_perl/)
-  
+
   if ! [[ $perl_version =~ ^[0-9.]+$ ]]; then
     echo " startTeXmacs.sh: Possible error in environment variables."
     echo " "
   fi
-  
+
   export PATH=/app/texlive/bin/${architecture_type}-linux:${PATH}
   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/app/texlive/lib/perl5/${perl_version}/${architecture_type}-linux/CORE
   export PERL5LIB=/app/texlive/lib/perl5/${perl_version}:/app/texlive/lib/perl5/site_perl/${perl_version}
@@ -18,7 +20,7 @@ export ASPELL_CONF='dict-dir /app/share/dicts'
 
 # Archivo de configuracion
 config_file="flat-config.conf"
-path_config_file="~/.TeXmacs"
+path_config_file=~/.TeXmacs
 IDN="0"
 if [[ -f "${path_config_file}/${config_file}" ]]; then
 
@@ -27,12 +29,12 @@ if [[ -f "${path_config_file}/${config_file}" ]]; then
     if [[ "$ID" == "$IDN" ]]; then
       exec texmacs "$@"
     else
-      echo "id=$IDN" > "${path_config_file}/${config_file}"
+      echo "ID=$IDN" > "${path_config_file}/${config_file}"
       exec texmacs /app/share/texmacs-flatpak/firts_run.tm
     fi
 else
-
-   echo "id=$IDN" > "${path_config_file}/${config_file}"
-   exec texmacs /app/share/texmacs-flatpak/firts_run.tm
+    touch "${path_config_file}/${config_file}"
+    echo "ID=$IDN" > "${path_config_file}/${config_file}"
+    exec texmacs /app/share/texmacs-flatpak/firts_run.tm
 fi
 
